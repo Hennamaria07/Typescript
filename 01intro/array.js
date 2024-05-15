1e7dbddab7728319f5e1ac28dd68fe23ea108b11
Henna Maria <hennamaria2001@gmail.com>
2024-05-15 14:53:46 +0530

    array basic added

---
+++ 01intro/array.js
@@ -0,0 +1,14 @@
+var superHeros = [];
+// const heroPower: number[] = []
+var heroPower = [];
+var allUsers = [
+    { name: "", isActive: true },
+    { name: "", isActive: true }
+];
+var MLModels = [
+    [255, 255, 255],
+    []
+];
+superHeros.push("spiderman");
+heroPower.push(2);
+allUsers.push({ name: "", isActive: true });
---
+++ 01intro/array.ts
@@ -0,0 +1,24 @@
+const superHeros: string[] = []
+// const heroPower: number[] = []
+const heroPower: Array<number> = []
+
+type User = {
+    name: string
+    isActive: boolean
+}
+
+const allUsers: User[] = [
+    {name: "", isActive: true},
+    {name: "", isActive: true}
+]
+
+const MLModels: number[][] = [
+    [255, 255, 255],
+    []
+]
+
+
+superHeros.push("spiderman")
+heroPower.push(2)
+
+allUsers.push({name: "", isActive: true})
